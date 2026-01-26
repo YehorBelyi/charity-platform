615bc9f6e61b9f852487b0c6a27834bc457dfc8f
Yehor Bilyi <egorwhite06@gmail.com>
2026-01-26 16:31:27 +0200

    init commit

---
+++ pages/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.views.generic import View
+
+# Create your views here.
+class HomePageView(View):
+    template_name = 'pages/index.html'
+
+    def get(self, request):
+        return render(request, self.template_name)
