66512b0e274899a6982610aeccfa6d2c276b6dc9
Dan23 <proidysvitdanylo@gmail.com>
2026-01-27 22:02:40 +0200

    Add login and registration pages. Create empty styles.css for future. Add urls to login and registration pages. Create basic class-views. Add '/' sign to urls in charity_platform/urls.py.

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
+from users.views import LoginView, SignUpView
 
 urlpatterns = [
-
+    path('login/', LoginView.as_view(), name='login'),
+    path('signup/', SignUpView.as_view(), name='signup'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from django.views import View
+
 
 # Create your views here.
+class LoginView(View):
+    def get(self, request):
+        return render(request, 'users/login.html')
+
+class SignUpView(View):
+    def get(self, request):
+        return render(request, 'users/signup.html')
+
+    # template_name = 'authentication/login.html'
+    # form_class = forms.LoginForm
+    #
+    # def get(self, request):
+    #     form = self.form_class()
+    #     message = ''
+    #     return render(request, self.template_name, context={'form': form, 'message': message})
+    #
+    # def post(self, request):
+    #     form = self.form_class(request.POST)
+    #     if form.is_valid():
+    #         user = authenticate(
+    #             username=form.cleaned_data['username'],
+    #             password=form.cleaned_data['password'],
+    #         )
+    #         if user is not None:
+    #             login(request, user)
+    #             return redirect('home')
+    #     message = 'Login failed!'
+    #     return render(request, self.template_name, context={'form': form, 'message': message})
