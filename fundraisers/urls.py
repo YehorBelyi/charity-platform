05153bcfa4139904cfe158e92e252345d0dca475
Anton <garynets.anton@gmail.com>
2026-01-28 20:08:16 +0200

    feat(fundraisers): add page for creating announcements

--- fundraisers/urls.py
+++ fundraisers/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("announcement/<int:announcement_id>/", views.fundraising_announcement, name="fundraising_announcement")
+    path("announcement/<int:announcement_id>/", views.fundraising_announcement, name="fundraising_announcement"),
+    path("create", views.CreateAnnouncementView.as_view(), name="create_announcement"),
 ]
--- fundraisers/views.py
+++ fundraisers/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import CreateView
 from .models import FundraisingAnnouncement
+from .forms import AddFundraisingAnnouncementForm
 
 def fundraising_announcement(request, announcement_id):
     announcement = get_object_or_404(FundraisingAnnouncement, pk=announcement_id)
@@ -8,3 +10,10 @@ def fundraising_announcement(request, announcement_id):
         "announcement": announcement
     }
     return render(request, "fundraisers/announcement.html", context)
+
+
+class CreateAnnouncementView(CreateView):
+    template_name = "fundraisers/create_announcement.html"
+    model = FundraisingAnnouncement
+    form_class = AddFundraisingAnnouncementForm
+    success_url = "/"
