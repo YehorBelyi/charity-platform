5882a07e5478b5831f50de7bb2c6dac96e64e733
Anton <garynets.anton@gmail.com>
2026-01-28 11:39:30 +0200

    feat(fundraisers): add FundraisingAnnouncement model

--- fundraisers/models.py
+++ fundraisers/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class FundraisingAnnouncement(models.Model):
+    title = models.CharField(max_length=300)
+    current_sum = models.DecimalField(max_digits=12, decimal_places=2, default=0)
+    target_sum = models.DecimalField(max_digits=12, decimal_places=2)
+    description = models.TextField()
+    photo = models.ImageField(upload_to="announcement_images/", null=True, blank=True)
+    operational_direction = models.CharField(max_length=100, null=True, blank=True)
+    unit = models.CharField(max_length=200, null=True, blank=True)
+    is_closed = models.BooleanField(default=False)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
