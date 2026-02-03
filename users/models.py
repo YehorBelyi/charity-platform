2fb1fc0a9e8405ac76deab8d6ac03dd8aba0c383
Dan23 <proidysvitdanylo@gmail.com>
2026-02-03 18:44:51 +0200

    refractor(ui): Minor changes in models.py.

--- users/models.py
+++ users/models.py
@@ -2,8 +2,6 @@ from django.utils import timezone
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-from django import forms
-
 
 # Create your models here.
 class CustomUser(AbstractUser):
