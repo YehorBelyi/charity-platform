4a666700599a93f0cf86be89cf6e1821ac81d0c5
Anton <garynets.anton@gmail.com>
2026-02-16 21:06:27 +0200

    chore(settings): Update login url, locale, timezone and add app

--- charity_platform/settings.py
+++ charity_platform/settings.py
@@ -51,7 +51,8 @@ INSTALLED_APPS = [
     'fundraisers',
     'users',
     'pages',
-    'verifications'
+    'verifications',
+    'units'
 ]
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
@@ -122,9 +123,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'uk-ua'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Kyiv'
 
 USE_I18N = True
 
@@ -139,6 +140,6 @@ STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-LOGIN_URL = '/login/'
+LOGIN_URL = '/users/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
