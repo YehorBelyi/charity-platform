72eba95570db117cdf49c7321d22ece9f8413310
Anton <garynets.anton@gmail.com>
2026-02-16 21:21:51 +0200

    feat(units): Implement units system

--- units/views.py
+++ units/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Unit
 
-# Create your views here.
+def unit(request, unit_id):
+    unit = get_object_or_404(Unit, pk=unit_id)
+
+    hierarchy = [unit]
+    parent = unit.parent
+    while parent:
+        hierarchy.append(parent)
+        parent = parent.parent
+    hierarchy.reverse()
+
+    context = {
+        "unit": unit,
+        "hierarchy": hierarchy
+    }
+    return render(request, "units/unit.html", context)
