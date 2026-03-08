af0db69117249ba583730c55e4e856417b55f39f
Yehor Bilyi <egorwhite06@gmail.com>
2026-03-08 21:12:00 +0200

    feat(payment): Add payment system

--- donations/urls.py
+++ donations/urls.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from django.urls import path
+from .views import SuccessfulPaymentView, CanceledPaymentView, SetPaymentView, CreateCheckoutSessionView
+from .webhooks import stripe_webhook_view
 
 urlpatterns = [
-
+    path('webhooks/stripe/', stripe_webhook_view, name='stripe-webhook'),
+    path('success/', SuccessfulPaymentView.as_view(), name='payment-success'),
+    path('cancellation/', CanceledPaymentView.as_view(), name='payment-canceled'),
+    path('create/<int:announcement_id>', SetPaymentView.as_view(), name='payment-set'),
+    path('create-checkout-session/<int:announcement_id>', CreateCheckoutSessionView.as_view(),
+         name='create-checkout-session'),
 ]
---
+++ donations/webhooks.py
@@ -0,0 +1,46 @@
+import stripe
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+
+from fundraisers.models import FundraisingAnnouncement
+from .models import Payment
+from django.conf import settings
+
+stripe.api_key = settings.STRIPE_SECRET_KEY
+webhook_secret = settings.STRIPE_WEBHOOK_SECRET
+
+@csrf_exempt
+def stripe_webhook_view(request):
+    payload = request.body
+    sig_header = request.META.get('HTTP_STRIPE_SIGNATURE')
+    event = None
+
+    try:
+        event = stripe.Webhook.construct_event(
+            payload, sig_header, webhook_secret
+        )
+    except ValueError as e:
+        return HttpResponse(status=400)
+    except stripe.error.SignatureVerificationError as e:
+        return HttpResponse(status=400)
+
+    if event['type'] == 'checkout.session.completed':
+        session = event['data']['object']
+
+        if event["type"] == "checkout.session.completed":
+            session = event["data"]["object"]
+
+            announcement_id = session["metadata"]["announcement_id"]
+            amount = session["metadata"]["amount"]
+
+            announcement = FundraisingAnnouncement.objects.get(
+                id=announcement_id
+            )
+
+            Payment.objects.create(
+                announcement=announcement,
+                amount=amount,
+                stripe_payment_intent=session["payment_intent"]
+            )
+
+    return HttpResponse(status=200)
