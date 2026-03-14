7e050b1579394ed9048051d4d37936dbf49711c6
keenan <ferqroyer@gmail.com>
2026-03-14 11:32:23 +0200

    feat(tests): Add unit tests

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,17 +1,19 @@
-# import pytest
-# from django.contrib.auth import get_user_model
+import pytest
+from django.contrib.auth import get_user_model
 
-# @pytest.mark.django_db
-# def test_create_user():
-#     User = get_user_model()
+@pytest.mark.django_db
+def test_create_user():
+    User = get_user_model()
 
-#     user = User.objects.create_user(
-#         username="testuser",
-#         email="test@example.com",
-#         password="testpassword123"
-#     )
+    user = User.objects.create_user(
+        username="testuser",
+        email="test@example.com",
+        password="testpassword123"
+    )
 
-#     assert user.id is not None
-#     assert user.username == "testuser"
-#     assert user.email == "test@example.com"
-#     assert user.check_password("testpassword123")
+    assert user.id is not None
+    assert user.username == "testuser"
+    assert user.email == "test@example.com"
+    assert user.check_password("testpassword123")
+
+# test
