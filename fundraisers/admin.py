cc85004bcf46e1451e25dc921272029c8a22816f
Anton <garynets.anton@gmail.com>
2026-03-22 11:43:14 +0200

    docs(fundraisers): Add documentation for fundraisers app

--- fundraisers/admin.py
+++ fundraisers/admin.py
@@ -1,4 +1,12 @@
+"""
+Django admin configuration for the fundraisers application.
+
+Registers models in the admin interface to allow staff users
+to view and manage fundraisers records.
+"""
+
 from django.contrib import admin
 from .models import FundraisingAnnouncement
 
+#: Register the FundraisingAnnouncement model in the Django Admin site.
 admin.site.register(FundraisingAnnouncement)
--- fundraisers/forms.py
+++ fundraisers/forms.py
@@ -5,14 +5,23 @@ from units.models import Unit
 from .models import FundraisingAnnouncement
 
 class AddFundraisingAnnouncementForm(forms.ModelForm):
+    """Form for creating announcements."""
+
+    #: Announcement title.
     title = forms.CharField(label="Назва збору:")
+    #: Amount needed to reach the fundraising goal.
     target_sum = forms.DecimalField(label="Ціль (ГРН):", min_value=0.01)
+    #: Main information about announcement.
     description = forms.CharField(label="Опис:", widget=forms.Textarea())
+    #: Photo representing the announcement.
     photo = forms.ImageField(label="Фото для збору:", required=False)
+    #: The operational direction where the supported unit is deployed.
     operational_direction = forms.CharField(label="Напрямок фронту:", required=False)
+    #: Supported unit.
     unit = forms.ModelChoiceField(queryset=Unit.objects.all(), label="Підрозділ", required=False)
 
     def __init__(self, *args, **kwargs):
+        """Configure the FormHelper for crispy-forms."""
         super().__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.form_method = "post"
@@ -34,9 +43,13 @@ class AddFundraisingAnnouncementForm(forms.ModelForm):
 
 
 class SearchForm(forms.Form):
+    """Form for searching announcements."""
+
+    #: Search query.
     search = forms.CharField(max_length=300, required=False, label="")
 
     def __init__(self, *args, **kwargs):
+        """Configure the FormHelper for crispy-forms."""
         super().__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.form_method = "get"
