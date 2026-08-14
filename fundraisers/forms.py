12d2dba33303a4ef4b2b25d7aa0871db83f5738f
Anton <garynets.anton@gmail.com>
2026-08-14 16:51:08 +0300

    feat: Add ordering to announcements list

--- fundraisers/forms.py
+++ fundraisers/forms.py
@@ -1,7 +1,7 @@
 """Forms for the fundraisers app"""
 
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Field, Submit, Layout
+from crispy_forms.layout import Field, Submit, Layout, Div, HTML
 from django import forms
 from units.models import Unit
 from .models import FundraisingAnnouncement
@@ -49,15 +49,37 @@ class SearchForm(forms.Form):
 
     #: Search query.
     search = forms.CharField(max_length=300, required=False, label="")
+    #: Select field for ordering fundraising announcements.
+    ordering = forms.ChoiceField(choices=[
+        ("popular", "Найбільш популярні"), ("newest", "Найновіші"), ("biggest", "Найбільші")
+    ], required=False, label="")
 
     def __init__(self, *args, **kwargs):
         """Configure the FormHelper for crispy-forms."""
         super().__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.form_method = "get"
-        self.helper.form_class = "my-4 w-100"
+        self.helper.form_class = "w-100"
 
         self.helper.layout = Layout(
-            Field("search", placeholder="Назва збору"),
-            Submit("", "Пошук")
+            Div(
+                Div(
+                    Field("search", placeholder="Назва збору", css_class="search-input", wrapper_class="mt-3"),
+                    css_class="flex-grow-1",
+                ),
+
+                HTML("""
+                    <button type="submit" class="search-button">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                          <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
+                        </svg>               
+                    </button>
+                    """),
+                css_class="d-flex gap-2"
+            ),
+
+            Div(
+            Field("ordering", wrapper_class="mt-3", onchange="this.form.submit()", css_class="ordering-select"),
+                css_class="ordering-select-container",
+            )
         )
